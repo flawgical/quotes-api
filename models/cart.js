4f269b6aa8f5ba9bc0c681bb03800035cf4deb9a
flawgical <shahzadkhan3iii7@gmail.com>
2020-01-14 09:16:24 -0600

    creates cart models and changes quantity to type number

--- models/cart.js
+++ models/cart.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const quoteSchema = new Schema({
+const cartSchema = new Schema({
     quantity: {type: Number},
     author: {type: String},
     image: {type: String},
@@ -12,4 +12,4 @@ const quoteSchema = new Schema({
   timestamps: true
 });
 
-module.exports = mongoose.model('Quote', quoteSchema);
+module.exports = mongoose.model('Cart', cartSchema);
