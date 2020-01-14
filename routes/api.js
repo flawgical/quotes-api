1f3cfc190a5ea845029cd0101f834ffeaa10af52
flawgical <shahzadkhan3iii7@gmail.com>
2020-01-14 09:47:54 -0600

    creates routes for cart model and controller

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const quotesCtrl = require('../controllers/api/quotes');
+const cartCtrl = require('../controllers/api/cart');
 
 /* GET /api/quotes */
 router.get('/quotes', quotesCtrl.index);
@@ -8,5 +9,12 @@ router.get('/quotes/:id', quotesCtrl.show);
 router.post('/quotes', quotesCtrl.create);
 router.delete('/quotes/:id', quotesCtrl.delete);
 router.put('/quotes/:id', quotesCtrl.update);
+
+/* GET /api/carts */
+router.get('/cart', cartCtrl.index);
+router.get('/cart/:id', cartCtrl.show);
+router.post('/cart', cartCtrl.create);
+router.delete('/cart/:id', cartCtrl.delete);
+router.put('/cart/:id', cartCtrl.update);
 
 module.exports = router;
