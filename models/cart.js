ee0764f155f4d12e15293c8338ff53a0d178f41d
flawgical <shahzadkhan3iii7@gmail.com>
2020-01-14 18:52:03 -0600

    adds itemID to the model

--- models/cart.js
+++ models/cart.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const cartSchema = new Schema({
+    itemId: {type: String},
     quantity: {type: Number},
     author: {type: String},
     image: {type: String},
